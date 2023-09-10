815c5da6793a07d19710c73fcf261b00aa388d39
khouloudAl2022 <khouloud.aloui@rigas.com.tn>
2023-09-10 13:47:06 +0100

    fisr commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react";
+import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-function App() {
+const adminRouting = adminRoutes.map((route) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    route.element && {
+      path: route.path,
+      element: <route.element />,
+      exact: route.exact,
+      name: route.name
+    }
+  )
+})
+const clientRouting = clientRoutes.map((route) => {
+  return (
+    route.element && {
+      path: route.path,
+      element: <route.element />,
+      exact: route.exact,
+      name: route.name
+    }
+  )
+})
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ClientLayout />,
+    children: clientRouting
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: '/admin',
+    element: <PrivateRoute><Layout /></PrivateRoute>,
+    children: adminRouting
+  },
+  {
+    path: '*',
+    element: <Page404 />
+  },
+
+]);
+function App() {
+  return <div className="App">
+ <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <Suspense fallback={Loading}>
+        <RouterProvider router={router} fallbackElement={Loading} />
+
+      </Suspense>
+  </div>;
 }
 
 export default App;
